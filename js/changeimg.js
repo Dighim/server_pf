36893d1ed73ef136c26ceaa7b9a840b241c5b046
Dighim <bmmulier@hotmail.fr>
2018-03-24 14:51:49 +0100

    Change img en dur

--- js/changeimg.js
+++ js/changeimg.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
-    $(".img").html($("<img src='"+$(".img").text()+"' class='img-thumbnail img-responsive'>"));
+$(document).ready(function () {
+    $(".img").html($("<img src='" + $(".img").text() + "' class='img-thumbnail img-responsive'>"));
     $(".img").show();
+    $(document).keydown(function(e){
+        e = e || window.event
+        var code = e.which || e.keyCode
+        if(code == 39) $(".img img").attr("src","https://www.wien.info/media/images/41161-campus-der-wirtschaftsuniversitaet-19to1.jpeg");
+    });
 });
